1d73c0793e429cded95a3000f692e4e2508a3fb7
yuriyfomin17 <fominyura2012@gmail.com>
2021-06-02 20:24:36 +0300

    learning

--- src/com/yuriy/overloading/Overloading.java
+++ src/com/yuriy/overloading/Overloading.java
@@ -4,18 +4,26 @@ import java.util.Random;
 
 public class Overloading {
     public static void main(String[] args) {
+        /* in package word public and private allows
+        * to set access to methods and fields
+        * e.g. inside package evey class can access
+        * public method of another class and
+        * same for the field*/
+        System.out.println(" Employer nexNextId=" + Employee.getNextNextId());
         /* Overloading occurs if the several methods have the same name but methods have
          * different parameters supplied to them */
         var messages = new StringBuilder();
         var todoList = new StringBuilder("To do:\n");
         Employee e2 = new Employee();
+        Employee.nextId += 1;
         System.out.println("Employer name is " + e2.getName() + Employee.getNextId());
     }
 }
 
 class Employee{
+    private static int nextNextId;
     public static int nextId;
-    String name;
+    private final String name;
     /* this is the other option to initialize the nextId
     * if it requires complex way to do it*/
     static
@@ -39,4 +47,5 @@ class Employee{
     public static int getNextId(){
         return nextId;
     }
+    public static int getNextNextId() {return nextNextId;}
 }
