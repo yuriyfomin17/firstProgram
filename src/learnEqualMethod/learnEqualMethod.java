32e2a184270d7e6b89d63b1bd3b0ec924683637e
yuriyfomin17 <fominyura2012@gmail.com>
2021-06-05 20:51:59 +0300

    learning

--- src/learnEqualMethod/learnEqualMethod.java
+++ src/learnEqualMethod/learnEqualMethod.java
@@ -2,9 +2,27 @@ package learnEqualMethod;
 
 public class learnEqualMethod {
     public static void main(String[] args){
+        Object obj = new Object();
         var employee = new Employee("Yuriy", 5000);
         var manger = new Manager("Yuriy", 5000);
-        var index = manger.equals(manger);
+        var manger2 = new Manager("Yuriy", 5000);
+
+        var index = employee.equals(manger);
+        System.out.println("Index = " + index);
+        System.out.println("Index = " + index);
+        /* equal method should be symmetric manager should be equal to employee
+        * and employee should be equal to manager but this is not true in terms
+        * and violate rules of OOP so can't use instance of if want to compare two objects
+        *
+        *
+        * In turn should override method equals of superclass Object
+        * Also, var manager = new Manager is not equal to var manager2 = new Manager
+        * since they refer to the references. Instead , should override the "equals method"
+        * of superclass object*/
+        var index1 = (manger instanceof Employee);
+        System.out.println("Index1 = " + index1);
+        var index2 = manger.equals(manger2);
+        System.out.println("Index 2 = " + index2);
     }
 
 }
@@ -28,6 +46,7 @@ class Employee{
         aPercent = aPercent / 100 + 1;
         this.salary = Math.round(this.salary * aPercent);
     }
+    /* Overriding method equals of class Object*/
    @Override public boolean equals(Object otherObject){
 
         // quick test to see if the objects are identical
@@ -36,12 +55,16 @@ class Employee{
         // must return false if the explicit parameter is null
         if(otherObject == null) return false;
 
+
         // if the classes don't match, they can't be equal
         if(getClass() != otherObject.getClass()){
             return false;
         }
+       // if the same class holds for all subclasses then  if (!(otherObject instanceof ClassName)) return false;
+       // als can use this thing but stick to top
+
         Employee other = (Employee) otherObject;
-        return name.equals(other.name) && salary == other.salary;
+        return name.equals(other.name) && salary.equals(other.salary);
     }
 }
 class Manager extends Employee{
---
+++ src/learnHashCode.java
@@ -0,0 +1,53 @@
+import java.util.Objects;
+
+public class learnHashCode {
+    public static void main(String[] args){
+        String word = "Hellor";
+        byte val  = 108 & 0xff;
+        System.out.println();
+        var index = word.charAt(word.length() - 1);
+        /* String method uses following algorithm to compute
+        * hashcode of the string*/
+        int hash = 0;
+        for(int i =0; i< word.length(); i++){
+            var ch = word.charAt(i);
+            hash = 31 * hash + word.charAt(i);
+        }
+        /* Note that hashcode for String s and String t is the same
+        * as the class String derives hashcode for those strings based
+        * on contents.
+        *
+        * While StringBuilder doesn't have hashCode method.
+        * and default  hashCode method in the Object class derives the
+        * hashcode from the object's memory address*/
+        var s = "OK";
+        var sb = new StringBuilder(s);
+        System.out.println(s.hashCode() + " " + sb.hashCode());
+        var t = new String("OK");
+        var tb = new StringBuilder(t);
+        System.out.println(t.hashCode() + " " + tb.hashCode());
+
+        var e = new Employee("Yuriy", 23);
+        System.out.println(e.getHashCode());
+    }
+}
+
+class Employee{
+    private final String  name;
+    private final Integer age;
+    private final Integer hashCode;
+    private final Integer hashCode2;
+    Employee(String aName, Integer anAge){
+        this.name = aName;
+        this.age = anAge;
+        /* First way of doing hashcode*/
+        this.hashCode = Objects.hash(this.name, this.age);
+        /* Second way of doing hashcode*/
+        this.hashCode2 = 7 * Objects.hashCode(this.name) + 11 * Objects.hashCode(this.age);
+        System.out.println(this.hashCode2);
+    }
+
+    public Integer getHashCode() {
+        return hashCode;
+    }
+}
