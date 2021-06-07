145a1b0fcf22acd62987414b6a3d010faf9f7091
yuriyfomin17 <fominyura2012@gmail.com>
2021-06-07 20:14:36 +0300

    learning

--- src/exampleClass.java
+++ src/exampleClass.java
@@ -5,11 +5,11 @@ public class exampleClass {
     public static void main(String[] args){
         var alice1 = new Employee("Alice Adams", 75000, 1987, 12, 15);
         var alice2 = alice1;
-        var alice3 = new Employee("Alice Adams",75000, 1987, 12, 1 );
+        var alice3 = new Employee("Alice Adams",75000, 1987, 12, 15 );
         var bob = new Employee("Bob Brandson", 50000, 1989, 10, 1);
         System.out.println("alice1 == alice2: " + (alice1 == alice2));
         System.out.println("alice1 == alice3: " + (alice1 == alice3));
-        System.out.println("alice1.equals(alice3): " + alice1.equals(alice3));
+        System.out.println("alice3.equals(alice1): " + alice3.equals(alice1));
         System.out.println("alice1.equals(bob): " + alice1.equals(bob));
         System.out.println("bob.toString(): " + bob);
 
