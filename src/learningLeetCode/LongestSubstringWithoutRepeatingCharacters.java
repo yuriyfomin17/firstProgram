1653a5979a4b0f132613ec7dcb17e7b7321e853a
yuriyfomin17 <fominyura2012@gmail.com>
2021-06-27 21:46:59 +0300

    learning

--- src/learningLeetCode/LongestSubstringWithoutRepeatingCharacters.java
+++ src/learningLeetCode/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,9 +1,13 @@
 package learningLeetCode;
 
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class LongestSubstringWithoutRepeatingCharacters {
+    /* Time complexity is O(2n) = O(n)
+    *  Space complexity is O(min(m, n)) as we need O(k) space for sliding window */
     static int lengthOfLongestSubstring(String s) {
         int start = 0;
         int end = 0;
@@ -23,8 +27,27 @@ public class LongestSubstringWithoutRepeatingCharacters {
         }
         return windowLength;
     }
+    /* Time complexity is O(n) = O(n)
+     *  Space complexity is O(min(m, n)) as we need O(k) space for sliding window */
+    static int lengthOfLongestSubstring2(String s) {
+        int start = 0;
+        int end = 0;
+        int windowLength = Integer.MIN_VALUE;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        while (end< s.length()){
+            Integer charCode = s.codePointAt(end);
+            var index = hashMap.get(charCode);
+            if(index!= null && index< end && index >=start){
+                start = index + 1;
+            }
+            windowLength = Math.max(end - start + 1, windowLength);
+            hashMap.put(charCode, end);
+            end +=1;
+        }
+        return windowLength;
+    }
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring2("abba"));
     }
 }
