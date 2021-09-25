6a5479feed8dc4ffb8a81f3f9aed064348a94f97
yuriyfomin17 <fominyura2012@gmail.com>
2021-09-25 15:11:22 +0300

    learning

---
+++ src/algorithms/matrixMultiplicator.java
@@ -0,0 +1,28 @@
+package algorithms;
+
+import java.util.Arrays;
+
+public class matrixMultiplicator {
+    public static void main(String[] args) {
+        int[][] array1 = {{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}};
+        int[][] array2 = {{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}};
+        int[][] array3 = new int[array1.length][array2.length];
+        multiplyMatrix(array1, array2, array3);
+    }
+
+    public static void multiplyMatrix(int[][] arr1, int[][] arr2, int[][] arr3) {
+        int rowLength = arr1.length;
+        int colLength = arr2[0].length;
+        if (rowLength != colLength) return;
+        for (int r = 0; r < rowLength; r++) {
+            for (int c = 0; c < colLength; c++) {
+                for (int i = 0; i < rowLength; i++) {
+                    arr3[c][r] += arr1[r][i] * arr2[i][c];
+                }
+
+            }
+        }
+        System.out.println(Arrays.deepToString(arr3));
+
+    }
+}
