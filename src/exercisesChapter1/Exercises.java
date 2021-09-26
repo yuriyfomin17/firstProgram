707239370631379d4d4acc632b15e55c2c5da2ab
yuriyfomin17 <fominyura2012@gmail.com>
2021-09-26 20:42:31 +0300

    learning

--- src/exercisesChapter1/Exercises.java
+++ src/exercisesChapter1/Exercises.java
@@ -1,8 +1,10 @@
 package exercisesChapter1;
 
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,7 +12,7 @@ import java.util.Map;
 public class Exercises {
 
     public static void main(String[] args) {
-        exe1120();
+        exe1121();
     }
 
     static public void exe111() {
@@ -180,6 +182,26 @@ public class Exercises {
             }
         }
         System.out.println(Math.log(Factorial.F(10, new HashMap<>())));
+    }
+    public static void exe1121(){
+        // simple printer program
+        // each line contain- ing a name and two integers and then uses printf() to print a table with a column of the names, the integers, and the result of dividing the first by the second, accurate to three decimal places.
+        // You could use a program like this to tabulate batting averages for baseball players or grades for students.
+        ArrayList<String> info = new ArrayList<>();
+        while (!StdIn.isEmpty()){
+            String string = StdIn.readLine();
+            info.add(string.trim());
+        }
+        for (String s: info) {
+            String[] args = s.split(",");
+            String name = args[0];
+            Integer num1 = Integer.parseInt(args[1]);
+            Integer num2 = Integer.parseInt(args[2]);
+            Double num3 = ((double) num1) / ((double) num2);
+            System.out.println("----------------------------");
+            StdOut.printf("| %s | %d | %d | %f |\n", name, num1, num2 , num3 );
+        }
+        System.out.println("----------------------------");
     }
 
 }
