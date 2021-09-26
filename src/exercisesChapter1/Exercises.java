3827aab29503403dba0cc4ea07af8e7753d8dbee
yuriyfomin17 <fominyura2012@gmail.com>
2021-09-26 15:36:42 +0300

    learning

--- src/exercisesChapter1/Exercises.java
+++ src/exercisesChapter1/Exercises.java
@@ -1,11 +1,13 @@
 package exercisesChapter1;
 
+import edu.princeton.cs.algs4.StdOut;
+
 import java.math.BigDecimal;
 
 public class Exercises {
 
     public static void main(String[] args) {
-        exe113(args);
+        exe119();
     }
     static public  void exe111(){
         System.out.println((0 + 15) / 2); // a 7 since converts it to int which truncates values
@@ -24,5 +26,52 @@ public class Exercises {
         int num3 = Integer.parseInt(args[2]);
         if (num3 == num2 && num2 == num1) System.out.println("equal");
         else System.out.println("not equal");
+    }
+    static public void exe116(){ // fibonacci sequence
+        int f = 0;
+        int g = 1;
+        for (int i = 0; i < 15; i++) {
+            StdOut.println(f);
+            f = f + g;
+            g = f - g;
+        }
+    }
+    static public void exe117(){
+        // a
+        double sqrtNum = 9.0;
+        double c = sqrtNum;
+        while (Math.abs(sqrtNum - c/ sqrtNum) > 0.001){
+            sqrtNum = (sqrtNum + c / sqrtNum) * 0.5;
+        }
+        StdOut.printf("%.5f\n", sqrtNum); // Newtonian method to find square root of number => 3.00009
+
+        // b
+        int sum = 0;
+        for (int i = 1; i < 1000; i++)
+            for (int j = 0; j < i; j++)
+                sum++;
+        StdOut.println(sum); // 499500
+
+        int N = 10;
+        int sum2 = 0;
+        for (int i = 1; i < 1000; i *= 2)
+            for (int j = 0; j < N; j++)
+                sum2++;
+        StdOut.println(sum2); // 100
+    }
+    static public void exe118(){
+        System.out.println("b"); // prints b
+        System.out.println("b" + "c"); // prints "bc"
+        System.out.println((char)('a' + 4)); // converts 'a' to 97 according to ascii table and add up 4 to 97 which is 101 then char converts 101 to 'e'
+    }
+    static  public void exe119(){
+        int num = 9;
+        System.out.println(Integer.toBinaryString(num));
+
+        String binaryNum = "";
+        for (int i = num; i> 0; i /= 2) {
+            binaryNum = (i % 2) + binaryNum;
+        }
+        System.out.println(binaryNum);
     }
 }
